a00e4fdfc5912e050e674739bfb1d6b9706c670a
stephanie-viti <svillegastito@gmail.com>
2026-01-26 10:24:18 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,17 @@
 print("hola, mundo")
+
+name = "brook"
+print("hola " + name)
+
+print(4+5)
+
+print(4-5)
+
+print(8/2) #este seria division
+
+print(8*2)
+
+print(2**3) #este seria elevado al cuadrado
+
+print(7+"8") #no puedeir entre comillas porque lo lee como texto
+#this will throw an error
